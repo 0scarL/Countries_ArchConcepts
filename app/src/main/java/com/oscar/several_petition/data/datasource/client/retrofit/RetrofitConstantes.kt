47f91c19bdee0278254f75fe33cc0c31b2655f56
0scarL <o_lizarazob@hotmail.com>
2024-09-10 11:41:00 -0500

    crea retrofit helper

---
+++ app/src/main/java/com/oscar/several_petition/data/datasource/client/retrofit/RetrofitConstantes.kt
@@ -0,0 +1,6 @@
+package com.oscar.several_petition.data.datasource.client.retrofit
+
+object RetrofitConstantes {
+    const val COUNTRY_URL = "https://restcountries.com/v3.1/"
+    const val REGION = "region/{region}"
+}
