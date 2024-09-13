07044476e6f60c89fc0a4d3cd3befe0ad0204bd3
0scarL <o_lizarazob@hotmail.com>
2024-09-13 12:15:02 -0500

    agrega constantes para nuevos endpoint

--- app/src/main/java/com/oscar/Countries/data/datasource/client/retrofit/RetrofitConstantes.kt
+++ app/src/main/java/com/oscar/Countries/data/datasource/client/retrofit/RetrofitConstantes.kt
@@ -3,4 +3,6 @@ package com.oscar.Countries.data.datasource.client.retrofit
 object RetrofitConstantes {
     const val COUNTRY_URL = "https://restcountries.com/v3.1/"
     const val REGION = "region/{region}"
-}
+    const val ALL = "all"
+    const val TRASLATION = "translation/{translation}"
+    }
